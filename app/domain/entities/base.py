3d7bb1e5c284494068c8c37e4348a660482a852a
levchuk <swankyyy1@gmail.com>
2025-05-05 10:47:24 +0300

    2. 37:59

--- app/domain/entities/base.py
+++ app/domain/entities/base.py
@@ -1,19 +1,29 @@
+import copy
 from dataclasses import dataclass, field
 from abc import ABC
 from uuid import uuid4
 from datetime import datetime
 
+from domain.events.base import BaseEvent
+
 
 @dataclass
 class BaseEntity(ABC):
-    oid: str = field(
-        default_factory=lambda: str(uuid4()),
-        kw_only=True)
+    oid: str = field(default_factory=lambda: str(uuid4()), kw_only=True)
 
     created_at: datetime = field(
         default_factory=datetime.now,
         kw_only=True,
     )
+    _events: list[BaseEvent] = field(default_factory=list, kw_only=True)
+
+    def pull_events(self) -> list[BaseEvent]:
+        register_events = copy(self._events)
+        self._events.clear()
+        return register_events
+
+    def register_event(self, event: BaseEvent) -> None:
+        self._events.append(event)
 
     def __hash__(self):
         return hash(self.oid)
